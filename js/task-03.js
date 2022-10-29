9f14424ce6cebb2ba32eb147bd0ec013e12ba1de
Kalenchenko Gleb <107788336+GKalenchenko@users.noreply.github.com>
2022-10-29 20:49:33 +0200

    update task-03

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,28 @@
-// const images = [
-//   {
-//     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'White and Black Long Fur Cat',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-//   },
-//   {
-//     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Group of Horses Running',
-//   },
-// ];
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+// --------------------------------------------------------------------------------------- //
+
+const ulRef = document.querySelector(`.gallery`);
+
+const markup = images.map(
+  image => `<li><img src="${image.url}", alt="${image.alt}", width= 500px;></img></li>`
+);
+
+ulRef.insertAdjacentHTML('beforeend', markup);
+
+ulRef.classList.add(flex);
+
+// Добавь с помощью classList флекс-класс на ul, а так же убери запятую в галерее ( предположительно с помощью join (" "))
