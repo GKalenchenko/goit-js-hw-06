47249e3eded981fe4b10e66d20568bce49e49934
Kalenchenko Gleb <107788336+GKalenchenko@users.noreply.github.com>
2022-10-31 14:23:49 +0200

    update task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector('.login-form');
 
+const formFunction = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    window.alert('Please make sure all fields are filled');
+  } else {
+    const infObj = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(infObj);
+    event.currentTarget.reset();
+  }
+};
+
+formRef.addEventListener('submit', formFunction);
