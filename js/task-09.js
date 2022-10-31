3bcaf95e356e90c3c273058f574711d7a829fbf6
Kalenchenko Gleb <107788336+GKalenchenko@users.noreply.github.com>
2022-10-31 17:54:52 +0200

    update task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// --------------------------------------------------------------------------------------- //
+
+const bodyRef = document.querySelector('body');
+const butttonRef = document.querySelector('.change-color');
+const spanRef = document.querySelector('.color');
+
+butttonRef.addEventListener('click', () => {
+  bodyRef.style.backgroundColor = `${getRandomHexColor()}`;
+  spanRef.textContent = `${bodyRef.style.backgroundColor}`;
+});
