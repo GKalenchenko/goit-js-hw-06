3f903a4f92b134480656d5b7df74a708c9c51312
Gleb Kalenchenko <gkalenchenko@gmail.com>
2022-11-14 12:48:30 +0300

    update task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// ---------------------------------------------------------------- //
+const formRef = document.getElementById('controls');
+const inputRef = formRef.querySelector('input');
+
+const buttonCreateRef = formRef.querySelector('[data-create]');
+const buttonDestroyRef = formRef.querySelector('[data-destroy]');
+
+const boxesListRef = document.getElementById('boxes');
+
+const inputApprove = () => {
+  createBoxes(`${inputRef.value}`);
+};
+
+buttonCreateRef.addEventListener('click', inputApprove);
+
+const destroyBoxes = () => {
+  boxesListRef.innerHTML = ' ';
+};
+
+buttonDestroyRef.addEventListener('click', destroyBoxes);
+
+let defaultWidth = 20;
+let defaultHeight = 20;
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    boxesListRef.insertAdjacentHTML(
+      'beforeend',
+      `<div style =" background-color: ${getRandomHexColor()}; width : ${(defaultWidth += 10)}px; height:${(defaultHeight += 10)}px;"></div>`
+    );
+  }
+}
